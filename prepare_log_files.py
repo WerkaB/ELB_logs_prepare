29b80ad3fbbf0f0758e5e0fe5df64903764b56f9
WerkaB <wero_66@wp.pl>
2021-01-28 13:03:48 +0100

    Comments added

--- prepare_log_files.py
+++ prepare_log_files.py
@@ -9,14 +9,13 @@ def download_and_unzip():
     bucket_name = os.getenv("bucket_name")
     download_directory = os.getenv("download_directory")
 
-    account_nr = os.getenv("account_nr")
-    bucket_az = os.getenv("bucket_AZ")
-    logs_date = os.getenv("logs_date")
+    account_nr = os.getenv("account_nr")    # provide env - plain account number of your LB
+    bucket_az = os.getenv("bucket_AZ")      # provide env in format eu-west-1
+    logs_date = os.getenv("logs_date")      # provide env in format YYYY/MM/DD
     prefix = os.path.join("AWSLogs", account_nr, "elasticloadbalancing", bucket_az, logs_date)
 
     s3_resource = boto3.resource('s3')
     my_bucket = s3_resource.Bucket(bucket_name)
-
 
     for my_bucket_object in my_bucket.objects.filter(Prefix=prefix):
         print(my_bucket_object)
@@ -34,6 +33,7 @@ def unzip_files(download_directory: str):
 
     for filename in filenames:
         abs_path_file_in = os.path.join(download_directory,filename)
+        # cutting last three chars (.gz)
         abs_path_file_out = os.path.join(destination_unzipped_directory,filename)[:-3]
 
         with gzip.open(abs_path_file_in, 'rb') as f_in:
